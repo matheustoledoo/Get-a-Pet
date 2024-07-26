bdc7663956efdef2b3e6ae98ae6c355cc1d57b0a
matheustoledoo <matheustoledo2312@gmail.com>
2024-07-26 15:26:32 -0300

    backend finished

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -17,13 +17,13 @@ router.post(
  router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
  router.get('/:id', PetController.getPetById)
  router.delete('/:id', verifyToken, PetController.removePetById)
-// router.patch(
-//   '/:id',
-//   verifyToken,
-//   imageUpload.array('images'),
-//   PetController.updatePet,
-// )
-// router.patch('/schedule/:id', verifyToken, PetController.schedule)
-// router.patch('/conclude/:id', verifyToken, PetController.concludeAdoption)
+ router.patch(
+  '/:id',
+   verifyToken,
+  imageUpload.array('images'),
+  PetController.updatePet,
+ )
+ router.patch('/schedule/:id', verifyToken, PetController.schedule)
+ router.patch('/conclude/:id', verifyToken, PetController.concludeAdoption)
 
 module.exports = router
